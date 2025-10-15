2232ad0eddb79eb1fde7161eebd3568fabdfdc63
baleunbit <vahyeon2514@sonline20.sen.go.kr>
2025-10-15 09:08:15 +0900

    없애

--- Room.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Room : MonoBehaviour
-{
-    [Header("Room Info")]
-    public int roomID;          // 방 고유 ID
-    public bool isStartRoom;    // 시작 방 여부
-    public bool isEndRoom;      // 보스/종료 방 여부
-}
